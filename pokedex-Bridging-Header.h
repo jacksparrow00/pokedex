98a72fa38b09448f7504ec89bd0800a7d619f3d9
jacksparrow00 <mehndiratta.nitish038@gmail.com>
2017-02-16 23:56:02 +0530

    Correction to bridging header file

---
+++ pokedex-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  pokedex
+//
+//  Created by Nitish on 16/02/17.
+//  Copyright © 2017 Nitish. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
+#import "FMDB.h"
